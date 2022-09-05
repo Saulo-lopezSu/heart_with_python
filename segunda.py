3ea8ee6f003468f451ada279d57fe4dff513c7c9
Saulo-lopezSu <73849544+Saulo-lopezSu@users.noreply.github.com>
2022-09-05 18:17:08 -0500

    Add files via upload

---
+++ segunda.py
@@ -0,0 +1,14 @@
+from turtle import *
+from time import sleep
+color('red')
+bgcolor('black')
+begin_fill()
+pensize(7)
+left(50)
+forward(143)
+circle(60,200)
+right(135)
+circle(60,200)
+forward(143)
+end_fill()
+sleep(5)
